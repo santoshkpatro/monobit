db821c0a188a76db3cf59549d774422e418e6edb
Santosh Kumar Patro <santosh@kpatro.in>
2026-02-20 02:51:07 +0530

    fix: static assets link

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,17 @@ import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'node:path'
 
-export default defineConfig(({ command }) => {
-  const isBuild = command === 'build'
-
-  return {
-    base: isBuild ? '/static/' : '/',
-    plugins: [vue(), vueDevTools()],
-    resolve: {
-      alias: {
-        '@': fileURLToPath(new URL('./monobit/web', import.meta.url)),
-      },
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./monobit/web', import.meta.url)),
     },
-    build: {
-      outDir: path.resolve(__dirname, 'monobit/static/monobit'),
-      assetsDir: '', // keep flat inside monobit folder
-      manifest: true,
-      emptyOutDir: true,
-    },
-  }
+  },
+  build: {
+    outDir: path.resolve(__dirname, 'monobit/static/monobit'),
+    assetsDir: '',
+    manifest: true,
+    emptyOutDir: true,
+  },
 })
